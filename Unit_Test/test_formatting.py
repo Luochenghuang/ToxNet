f0d012fc4dfd9f2dcd63368b8a4deefd31dafc0f
Sijia Xiao <lenaxiao@uw.edu>
2018-06-29 01:53:41 -0700

    bug fixed

--- Unit_Test/test_formatting.py
+++ Unit_Test/test_formatting.py
@@ -1,26 +1,29 @@
+import sys
+sys.path.insert(0, '../chem_scripts') # add file to be tested
+
 import numpy as np
 import pandas as pd
 import chem_scripts 
 
 def test_cs_load_csv():
-    chem_scripts.cs_load_csv("tox_niehs_int_nontoxic_rdkit.csv")
+    X, y = chem_scripts.cs_load_csv("../data/tox_niehs_int_nontoxic_rdkit.csv")
     try:
         chem_scripts.cs_load_csv(123)
     except(Exception):
         pass
     else:
         raise Exception("Input error. String expected")
-        
+    
+
     assert X.shape == (828,187), 'X shape error'
     assert y.shape == (828,), 'y shape error'
     
     assert type(X) == np.ndarray, 'X type error'
     assert type(y) == np.ndarray, 'y type error'
-    
-    return
+
 
 def test_cs_load_smiles():
-    chem_scripts.cs_load_smiles("tox_niehs_int_nontoxic_smiles.csv")
+    X, y = chem_scripts.cs_load_smiles("../data/tox_niehs_int_nontoxic_smiles.csv")
     try:
         chem_scripts.cs_load_smiles(123)
     except(Exception):
@@ -33,8 +36,7 @@ def test_cs_load_smiles():
     
     assert type(X) == np.ndarray, 'X type error'
     assert type(y) == np.ndarray, 'y type error'
-    
-    return
+
 
 def test_cs_create_dict():
     characters = set()
@@ -42,8 +44,8 @@ def test_cs_create_dict():
     maxlen = 0
     total_lines = 0
     
-    X, y = chem_scripts.cs_load_smiles('tox_niehs_tv_verytoxic_smiles.csv')
-    X_test, y_test = chem_scripts.cs_load_smiles('tox_niehs_int_verytoxic_smiles.csv')
+    X, y = chem_scripts.cs_load_smiles('../data/tox_niehs_tv_verytoxic_smiles.csv')
+    X_test, y_test = chem_scripts.cs_load_smiles('../data/tox_niehs_int_verytoxic_smiles.csv')
     
     characters, char_table, char_lookup = chem_scripts.cs_create_dict(X, X_test)
     
@@ -59,7 +61,7 @@ def test_cs_create_dict():
     return
 
 def test_cs_prep_data_y():
-    X_test, y_test = chem_scripts.cs_load_csv('tox_niehs_int_nontoxic_rdkit.csv')
+    X_test, y_test = chem_scripts.cs_load_csv('../data/tox_niehs_int_nontoxic_rdkit.csv')
     y_test, _ = chem_scripts.cs_prep_data_y(y_test, tasktype='classification')
     
     assert y_test.shape == (828, 2), 'shape error, set expected'
@@ -72,7 +74,7 @@ def test_prep_symbols():
     characters = set()
     maxlen = 0
     total_lines = 0
-    X, y = chem_scripts.cs_load_smiles('tox_niehs_int_nontoxic_smiles.csv', smiles_cutoff=250)
+    X, y = chem_scripts.cs_load_smiles('../data/tox_niehs_int_nontoxic_smiles.csv', smiles_cutoff=250)
     X_sample = X
     characters, maxlen, total_lines = chem_scripts.prep_symbols(X_sample, characters, maxlen, total_lines)
     
